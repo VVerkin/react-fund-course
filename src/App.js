3cc26441d2e8d1c2bbe7894dd8faa27a55895e0b
Vasilii <v.verkin@gmail.com>
2026-03-05 17:15:50 +0300

    добавляет стили

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, {useState} from 'react';
 import Counter from './components/Counter';
 import ClassCounter from './components/ClassCounter';
+import './styles/App.css';
 
 function App() {
   const [value, setValue] = useState('Текст в инпуте')
 
   return (
     <div className="App">
-      <ClassCounter/>
+      <div className="post">
+        <div clssName="post__content">
+          <strong>1. Javascript</strong>
+          <div>
+            Javascript - язык програмирования
+          </div>
+        </div>
+        <div className="post__btns">
+          <button>Удалить</button>
+        </div>
+      </div>
     </div>
   );
 }
