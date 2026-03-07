a547840df609725eac1dc5f5da115006064e5c06
vasilii <v.verkin@gmail.com>
2026-03-07 11:07:45 +0300

    добавляет пост через состояние с объектом

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState, useRef} from 'react';
+import {useState} from 'react';
 import './styles/App.css';
 import PostList from './components/PostList';
 import MyButton from './components/Ui/button/MyButton'
@@ -13,19 +13,12 @@ function App() {
     {id: 3, title: 'Javascript 3', body: 'Description'},
   ]);
 
-  const [title, setTitle] = useState('')
-  const [body, setBody] = useState('')
+  const [post, setPost] = useState({title: '', body: ''}) // Создаем состояние с объектом, в котором сразу инициализируем пустые строки
 
   const addNewPost = (e) => {
     e.preventDefault()
-    const newPost = {
-      id: Date.now(),
-      title,
-      body,
-    }
-    setPosts([...posts, newPost]);
-    setTitle('');
-    setBody('');
+    setPosts([...posts, {...post, id: Date.now()}]) // Создаем новый объект в массиве, разворачиваем информацию из поста и добавляем id
+    setPost({title: '', body: ''})
   }
 
   return (
@@ -33,14 +26,14 @@ function App() {
     <form>
     {/*Управляемый контент*/}
       <MyInput
-        value={title}
-        onChange={e => setTitle(e.target.value)}
+        value={post.title}
+        onChange={e => setPost({...post, title: e.target.value})} // Вызываем setPost и передаем туда объект, в котором разворачиваем старый Post и перезатираем нужное нам поле в конкретно этом инпуте
         type="text"
         placeholder="Название поста" 
       />
       <MyInput
-        value={body}
-        onChange={e => setBody(e.target.value)}
+        value={post.body}
+        onChange={e => setPost({...post, body: e.target.value})} // Аналогично изменяем нужное нам поле
         type="text"
         placeholder="Описание поста" />
       <MyButton onClick={addNewPost}>Создать пост</MyButton>
