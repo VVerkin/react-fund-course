fd65e8562a2e49b0236c4783d054b45f052e8429
Vasilii <v.verkin@gmail.com>
2026-03-16 16:55:29 +0300

    реализует удаление поста

--- src/App.js
+++ src/App.js
@@ -14,11 +14,15 @@ function App() {
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
   }
+  // Получает post из дочернего компонента
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
 
   return (
     <div className="App">
       <PostForm create={createPost}/>
-      <PostList posts={posts} title="Список постов 1"/>
+      <PostList remove={removePost} posts={posts} title="Список постов 1"/>
     </div>
   );
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,3 +1,4 @@
+import MyButton from './Ui/button/MyButton'
 
 const PostItem = (props) => {
   return (
@@ -9,7 +10,9 @@ const PostItem = (props) => {
           </div>
         </div>
         <div className="post__btns">
-          <button>Удалить</button>
+          <MyButton onClick={() => props.remove(props.post)}>
+            Удалить
+          </MyButton>
         </div>
       </div>
   );
