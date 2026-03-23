143c6ea6c45a0677e72693423ac0bb8e470ef42b
Vasilii <v.verkin@gmail.com>
2026-03-23 16:43:42 +0300

    реализует сортировку

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import MySelect from './components/Ui/select/MySelect';
 function App() {
 
   const [posts, setPosts] = useState([
-    {id: 1, title: 'Javascript', body: 'Description'},
-    {id: 2, title: 'Javascript 2', body: 'Description'},
-    {id: 3, title: 'Javascript 3', body: 'Description'},
+    {id: 1, title: 'аа', body: 'бб'},
+    {id: 2, title: 'гг 2', body: 'аа'},
+    {id: 3, title: 'вв 3', body: 'хх'},
   ]);
-
+  // Реализуем двустороннее связывание
+  const [selectedSort, setSelectedSort] = useState('')
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
   }
@@ -20,19 +21,26 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+  }
+
   return (
     
     <div className="App">
       <PostForm create={createPost}/>
       <hr style={{margin: '15px 0'}}></hr>
       <div>
-      <MySelect 
-        defaultValue="Сортировка"
-        options={[ 
-          {value: 'title', name: 'По названию'},
-          {value: 'body', name: 'По описанию'},
-        ]}
-      />
+        <MySelect 
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Сортировка"
+          options={[ 
+            {value: 'title', name: 'По названию'},
+            {value: 'body', name: 'По описанию'},
+          ]}
+        />
       </div>
 
       {
--- src/components/Ui/select/MySelect.jsx
+++ src/components/Ui/select/MySelect.jsx
@@ -1,7 +1,13 @@
 // Компонент принимает массив опций и дефолтное значение, и реализуем двустороннее связывание через управляемый компонент
 const MySelect = ({options, defaultValue, value, onChange}) => {
   return (
-    <select>
+    // select value и onChange для слежки за изменением компонента
+    <select
+        value={value}
+        //onChange позволяет следить за изменениями 
+        onChange={event => onChange(event.target.value)}
+        >
+        
         <option disabled value="">{defaultValue}</option>
         {/* Итерируемся по массиву опций и для каждой опции*/}
         {options.map(option =>
